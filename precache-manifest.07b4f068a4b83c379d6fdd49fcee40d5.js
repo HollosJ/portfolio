edc7b96cbf7222133a2cc7d7066cacee213fba9a
HollosJ <hollosj99@gmail.com>
2020-07-04 22:40:22 +0100

    Updates

---
+++ precache-manifest.07b4f068a4b83c379d6fdd49fcee40d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc96d14d6a1ba509ee7702babb2af595",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51233299a0603d2a1376",
+    "url": "/static/css/main.92379221.chunk.css"
+  },
+  {
+    "revision": "6be47fc2fbd0b99d140a",
+    "url": "/static/js/2.cc8e8cc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc8e8cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51233299a0603d2a1376",
+    "url": "/static/js/main.115b4775.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "d6b95c2adccc294f1e0512dcd13f43b7",
+    "url": "/static/media/down.d6b95c2a.svg"
+  },
+  {
+    "revision": "8422ddd62b106417d9884b313f8a399c",
+    "url": "/static/media/ps-logo.8422ddd6.svg"
+  }
+]);
