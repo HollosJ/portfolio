10caed3e4467463f81a8c6737c02c22529029605
HollosJ <hollosj99@gmail.com>
2020-11-30 16:50:32 +0000

    💄 Contact section styling update + added header

--- src/components/bottom/Bottom.js
+++ src/components/bottom/Bottom.js
@@ -4,6 +4,9 @@ import Contact from "./Contact";
 const Bottom = () => {
   return (
     <div className="Bottom">
+      <div className="Header">
+        <h1>Contact me</h1>
+      </div>
       <Contact />
     </div>
   );
