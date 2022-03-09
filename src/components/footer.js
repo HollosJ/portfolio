de6c026b0899d827471db8a079b651a54baca330
HollosJ <hollosj99@gmail.com>
2022-03-09 22:46:05 +0000

    Footer padding

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="w-screen mt-4 bg-black">
-      <div className="container px-4 mx-auto text-center text-white">
+      <div className="container px-4 py-2 mx-auto text-center text-white">
         Copyright © {new Date().getFullYear()} James Hollos. All rights
         reserved.
       </div>
