08fcac80ebaeb5f8412391c112791eaef6290990
HollosJ <hollosj99@gmail.com>
2022-03-10 00:59:28 +0000

    Added subheading

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,10 +4,13 @@ import { AiOutlineArrowDown } from 'react-icons/ai';
 const Intro = () => {
   return (
     <div className="relative flex flex-col items-center justify-center h-screen max-h-screen">
-      <div className="relative group">
-        <h1 className="relative text-3xl font-black text-center bg-jrey md:text-5xl">
+      <div className="relative flex flex-col items-center group">
+        <h1 className="relative text-3xl font-black text-center md:text-5xl">
           James Hollos
         </h1>
+        <h2 className="text-lg font-thin md:mt-2 md:text-3xl text-jurple">
+          Front-end Developer
+        </h2>
       </div>
       <a
         href="#about"
