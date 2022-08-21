9e99967262577f76a98456ebd6db57d401706934
HollosJ <hollosj99@gmail.com>
2022-08-21 22:38:41 +0100

    Refactor links styles

--- src/components/links.js
+++ src/components/links.js
@@ -3,49 +3,48 @@ import { BsLinkedin } from 'react-icons/bs';
 import { IoMdDocument, IoLogoCodepen, IoLogoGithub } from 'react-icons/io';
 
 const links = () => {
+  const styles = {
+    link: 'flex flex-col items-center p-2 text-2xl transition rounded-sm focus:text-jurple hover:text-jurple hover:outline-jurple hover:outline hover:outline-2 focus:outline-jurple focus:outline focus:outline-2',
+    icon: 'mt-2 text-xs text-center md:text-base',
+  };
+
   return (
     <div className="p-4 mx-auto mt-8 bg-black">
       <div className="container max-w-4xl mx-auto">
         <ul className="flex flex-wrap text-4xl text-white justify-evenly">
           {/* LinkedIn */}
           <a
-            className="flex flex-col items-center p-2 text-2xl transition rounded-sm focus:text-jurple hover:text-jurple hover:outline-jurple hover:outline hover:outline-2 focus:outline-jurple focus:outline focus:outline-2"
+            className={styles.link}
             href="https://www.linkedin.com/in/jameshollos/"
             target="_blank"
           >
             <BsLinkedin />
-            <span className="mt-2 text-xs text-center md:text-base ">
-              LinkedIn
-            </span>
+            <span className={styles.icon}>LinkedIn</span>
           </a>
           {/* GitHub */}
           <a
-            className="flex flex-col items-center p-2 text-2xl transition rounded-sm focus:text-jurple hover:text-jurple hover:outline-jurple hover:outline hover:outline-2 focus:outline-jurple focus:outline focus:outline-2"
+            className={styles.link}
             href="https://github.com/HollosJ"
             target="_blank"
           >
             <IoLogoGithub />
-            <span className="mt-2 text-xs text-center md:text-base ">
-              Github
-            </span>
+            <span className={styles.icon}>Github</span>
           </a>
           <a
-            className="flex flex-col items-center p-2 text-2xl transition rounded-sm focus:text-jurple hover:text-jurple hover:outline-jurple hover:outline hover:outline-2 focus:outline-jurple focus:outline focus:outline-2"
+            className={styles.link}
             href="https://codepen.io/HollosJ"
             target="_blank"
           >
             <IoLogoCodepen />
-            <span className="mt-2 text-xs text-center md:text-base ">
-              Codepen
-            </span>
+            <span className={styles.icon}>Codepen</span>
           </a>
           <a
-            className="flex flex-col items-center p-2 text-2xl transition rounded-sm focus:text-jurple hover:text-jurple hover:outline-jurple hover:outline hover:outline-2 focus:outline-jurple focus:outline focus:outline-2"
+            className={styles.link}
             href="https://drive.google.com/file/d/1025ja3ser6s5GgM5G2Btenl25hoAtggZ/view?usp=sharing"
             target="_blank"
           >
             <IoMdDocument />
-            <span className="mt-2 text-xs text-center md:text-base ">CV</span>
+            <span className={styles.icon}>CV</span>
           </a>
         </ul>
       </div>
